3d753d3516aa5201614e11df196c5c914cb32e4b
Rohan Shirsat <84229173+rohanshirsat31@users.noreply.github.com>
2024-02-10 20:14:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import { fileURLToPath } from 'url';
 
 dotenv.config();
 
-connectDB(uri, process.env.PORT);
+connectDB();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
